89f5672bbe16c154e7144565c7b2e48a50f96a93
Sione Tenifa <johntenifa@yahoo.com>
2019-03-15 09:27:42 -0600

    in-line style

--- src/Game.js
+++ src/Game.js
@@ -1,72 +1,130 @@
 import React from 'react'
-import { Button, Table, } from "semantic-ui-react";
+import { Table, Image, Container } from "semantic-ui-react";
 import './App.css';
+import Paper from './paper.png'
 import Rock from './rock.png'
+import Scissors from './scissors.jpeg'
 
 
 class Game extends React.Component{
   
+  state = { option: ' ', computer: '', 
+            user: '', win: 0, 
+            lose: 0,}
   
-  
-
   handleRock = () => {
-    var choices = ["rock", "paper", "scissors"]
-
-    var choice = "rock";
-    var rand = choices[Math.floor(Math.random() * choices.length)];
-    document.getElementById('two').innerHTML = "User: " + choice + ' ' + "Computer: " + rand
-  
-    if (choice == rand){
-      document.getElementById('one').innerHTML = 'Its a tie!';
-      return { showClock: !state.showClock, }
-    }
-    if (rand == "scissors") {
-      document.getElementById('one').innerHTML = 'You Win!';
-      return { showClock: !state.showClock, }
-    }
-    if (rand == "paper"){
-      document.getElementById('one').innerHTML = 'You lose';
-      return { showClock: !state.showClock, }
-    }
-    
-    
+    this.setState( state => {
+      let choices = ["rock", "paper", "scissors"]
+      let choice = "rock";
+      let rand = choices[Math.floor(Math.random() * choices.length)];
+      if (choice == rand){
+        return { option: "TIE", computer: rand, user: choice, }
+      }
+      if (rand == "scissors") {
+        return { option: "You Win!", computer: rand, user: choice,  }
+      }
+      if (rand == "paper"){
+        return { option: "You Lose!", computer: rand, user: choice, }
+      }
+    }) 
   }
-
   handlePaper = () => {
+    this.setState( state => {
+      let choices = ["rock", "paper", "scissors"]
+      let choice = "paper";
+      let rand = choices[Math.floor(Math.random() * choices.length)];
+      if (choice == rand){
+        // return { option: "TIE" }
+        return { option: "TIE", computer: rand, user: choice,   }
 
+      }
+      if (rand == "rock") {
+        // return { option: "You Win" }
+        return { option: "You Win!", computer: rand, user: choice,  }
+
+      }
+      if (rand == "scissors"){
+        return { option: "You Lose!", computer: rand, user: choice,  }
+      }
+    }) 
   }
 
   handleScissors = () => {
-
+    this.setState( state => {
+      let choices = ["rock", "paper", "scissors"]
+      let choice = "scissors";
+      let rand = choices[Math.floor(Math.random() * choices.length)];
+      if (choice == rand){
+        return { option: "TIE", computer: rand, user: choice, }
+      }
+      if (rand == "paper") {
+        return { option: "You Win", computer: rand, user: choice, }
+      }
+      if (rand == "rock"){
+        return { option: 'You Lose', computer: rand, user: choice,  }
+      }
+    }) 
   }
   render(){
     return(
     <header className='App-header'>
       <Table.Row>
         <Table.Cell>
-          <Button onClick={(e) => this.handleRock(e)}> Rock </Button>
-  
-  
-          <Button onClick={(e) => this.handlePaper(e)}> Paper </Button>
+          {/* <Image src={Rock} onClick={(e) => this.handleRock(e)}/>
+          <Image src={Paper} onClick={(e) => this.handlePaper(e)}/>
+          <Image src={Scissors} onClick={(e) => this.handleScissors(e)}/> */}
 
-  
-          <Button onClick={(e) => this.handleScissors(e)}> Scissors </Button>
-
-  
+          {/* <Button src={Rock} onClick={(e) => this.handleRock(e)}> Rock </Button>
+          <Button src={Paper} onClick={(e) => this.handlePaper(e)}> Paper </Button>
+          <Button src={Scissors} onClick={(e) => this.handleScissors(e)}> Scissors </Button> */}
         </Table.Cell>
       </Table.Row> 
+          <Container style={{display: 'flex', flexDirection: 'row', justifyContent: 'center',  }}>
+              <Image src={Rock} onClick={(e) => this.handleRock(e)}/>
+              <Image src={Paper} onClick={(e) => this.handlePaper(e)}/>
+              <Image src={Scissors} onClick={(e) => this.handleScissors(e)}/>
+          </Container >
+
+      <p>{this.state.option}</p>
+      <p>You Chose-{this.state.user}</p>
+      <p>Computer Chose-{this.state.computer}</p>
+
     </header>
 
     )
-  }
+  }    
+}
+export default Game; 
   
+  
+
+  
+
+  
+  
+
  
 
   
 
 
-}
+  
 
 
 
-export default Game; 
+
+
+    
+      
+      
+      
+  
+
+  
+
+
+
+
+    
+    
+
